baa8a245614219acd9559f4c6d4ca50219a8b272
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-12-15 16:29:57 -0300

    feat: adiciona testes para classe "Product" implementa testes para verificar se a função "Products" em "inventory_report/inventory/products.py" validando se os valores enviados estão sendo usados na classe

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,30 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+
+mock = {
+  "id": 10,
+  "nome_do_produto": "Nome test",
+  "nome_da_empresa": "Empresa teste",
+  "data_de_fabricacao": "2022-12-15",
+  "data_de_validade": "2025-12-15",
+  "numero_de_serie": "999999999999",
+  "instrucoes_de_armazenamento": "Local seco"
+}
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+      mock["id"],
+      mock["nome_do_produto"],
+      mock["nome_da_empresa"],
+      mock["data_de_fabricacao"],
+      mock["data_de_validade"],
+      mock["numero_de_serie"],
+      mock["instrucoes_de_armazenamento"]
+    )
+
+    assert mock["nome_do_produto"] in product.__repr__()
+    assert mock["data_de_fabricacao"] in product.__repr__()
+    assert mock["nome_da_empresa"] in product.__repr__()
+    assert mock["data_de_validade"] in product.__repr__()
+    assert mock["instrucoes_de_armazenamento"] in product.__repr__()
