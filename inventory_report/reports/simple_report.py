7a3725a50e3de53e8ba2900f4e5e75004e0c3aae
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-12-24 00:53:31 -0300

    feat: requisito 2 implementa a classe "simpleReport implementa a funcionalidade para que a classe receba um dicionario de produtos e retorne a data de fabricacao mais antiga, a validade mais proxima e a empresa com mais produtos

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,32 @@
+from datetime import date
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(products_data):
+        date_today = str(date.today())
+
+        manufacturing_dates = sorted([
+                                product["data_de_fabricacao"]
+                                for product in products_data
+                              ])
+
+        validity_dates = sorted([
+                        product["data_de_validade"]
+                        for product in products_data
+                        if product["data_de_validade"] >= date_today
+                      ])
+
+        companies = [
+                        product["nome_da_empresa"]
+                        for product in products_data
+                      ]
+
+        most_common_company = Counter(companies).most_common(1)
+
+        return (
+            f"Data de fabricação mais antiga: {manufacturing_dates[0]}\n"
+            f"Data de validade mais próxima: {validity_dates[0]}\n"
+            f"Empresa com mais produtos: {most_common_company[0][0]}"
+            )
