78093cec1cdbff281a55352be44fb6b36e9a113f
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-12-29 17:32:11 -0300

    fix: requisito 4 - erro de complexidade do metodo "import_data" fiz um metodo complementar para ler o arquivo e converte-lo em uma lista de dicionario e a funcao principal apenas usa esses dados para gera o relatorio

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -7,21 +7,25 @@ import json
 
 class Inventory:
     @classmethod
-    def import_data(cls, path, report_type):
-        list_of_dict = ""
-
+    def read_file(cls, path):
+        file_data = ""
         with open(path, 'r') as file:
             if path.endswith(".csv"):
-                list_of_dict = list(DictReader(file))
+                file_data = list(DictReader(file))
             elif path.endswith(".xml"):
                 dict_file = xmltodict.parse(file.read())
-                list_of_dict = dict_file["dataset"]["record"]
+                file_data = dict_file["dataset"]["record"]
             elif path.endswith(".json"):
-                list_of_dict = json.load(file)
+                file_data = json.load(file)
+        return file_data
+
+    @classmethod
+    def import_data(cls, path, report_type):
+        list_of_products_in_dict = cls.read_file(path)
 
         if report_type.lower() == "simples":
-            return SimpleReport.generate(list_of_dict)
+            return SimpleReport.generate(list_of_products_in_dict)
         elif report_type.lower() == "completo":
-            return CompleteReport.generate(list_of_dict)
+            return CompleteReport.generate(list_of_products_in_dict)
         else:
             return "Invalid Report Type"
