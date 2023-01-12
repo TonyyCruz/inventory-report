aecd5d8537e3f21760e104968e49ce39cf5ee2bd
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-01-11 21:50:40 -0300

    feat: requisito 11 - implementa parcialmente a funcao main ela recebe dois parametros por "sys", um path e um tipo de relatorio, depois gera um relatorio com os dados equivalentes aos dados recebidos

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,29 @@
-def main():
-    pass
+import sys
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+
+def main(path, report):
+    print("------------ARGUMENTOS----", path, report)
+    if path.endswith(".csv"):
+        importer = CsvImporter
+    elif path.endswith(".xml"):
+        importer = XmlImporter
+    elif path.endswith(".json"):
+        importer = JsonImporter
+    else:
+        print('A extensão do arquivo deve ser ".csv", ".json" ou ".xml"')
+        raise ValueError("Arquivo inválido")
+
+    inventory_report = InventoryRefactor(importer)
+    return inventory_report.import_data(path, report)
+
+
+if len(sys.argv) < 2:
+    sys.stderr.write("Verifique os argumentos")
+else:
+    file_path = sys.argv[0]
+    report_type = sys.argv[1]
+    main(file_path, report_type)
